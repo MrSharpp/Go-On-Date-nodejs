c23626dbd5cfbf1a49ee5f6a6f7b7320fa71682a
Amir Alam <sharpprogrammer2018@gmail.com>
2022-01-20 05:03:56 -0800

    Update nft.js

--- controllers/nft.js
+++ controllers/nft.js
@@ -13,7 +13,7 @@ const mintImage = (req, res) => {
     var ipfsHash = "https://gateway.pinata.cloud/ipfs/" + req.body.ipfsHash;
     console.log(ipfsHash);
     exec('pwd', (error, stdout, stderr) => {
-      console.log(stderr);
+      console.log(stdout);
     });
     exec('ts-node "'+process.env.METAPLEXPATH+'" mint -e devnet -k ./devnet.json -u "'+ipfsHash+'"', (error, stdout, stderr) => {
         if (error) {
